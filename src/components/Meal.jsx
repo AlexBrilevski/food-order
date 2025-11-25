3694e514e38fb72cdbdd792c4e542422709e2de0
Alex Brilevski <brilevski.alex@gmail.com>
2025-11-25 20:03:14 +0300

    Outputting meal description was fixed

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,15 +1,15 @@
-export default function Meal({ id, image, name, decription, price }) {
+export default function Meal({ id, image, name, description, price }) {
   return (
     <div className="meal-item">
       <article>
         <img src={`http://localhost:3000/${image}`} alt={name} />
         <div>
           <h3>{name}</h3>
-          <p className="meal-item-description">
-            {decription}
-          </p>
           <p className="meal-item-price">
             {price}
+          </p>
+          <p className="meal-item-description">
+            {description}
           </p>
         </div>
         <p className="meal-item-actions">
