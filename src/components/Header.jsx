b13fda073dad169fd6a117bf16ef82cd4ce4e52e
Alex Brilevski <brilevski.alex@gmail.com>
2025-11-26 20:54:00 +0300

    Code formatting in the header component was fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,17 @@ import logo from '../assets/logo.jpg';
 import Button from './ui/Button.jsx';
 
 export default function Header() {
-	return (
-		<header id="main-header">
-			<div id="title">
-				<img src={logo} alt="A restaurant" />
-				<h1>React FoodOrder</h1>
-			</div>
-			<nav>
-				<Button textOnly>
-					Cart (0)
-				</Button>
-			</nav>
-		</header>
-	);
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logo} alt="A restaurant" />
+        <h1>React FoodOrder</h1>
+      </div>
+      <nav>
+        <Button textOnly>
+          Cart (0)
+        </Button>
+      </nav>
+    </header>
+  );
 }
