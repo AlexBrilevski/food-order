1d32a9d8d9b2211904d1a4334d55bcbff088e54f
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-01 21:02:08 +0300

    Custom Input component and outputting the checkout form were added

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,17 +1,38 @@
 import { useContext } from "react";
 import Modal from "./ui/Modal";
 import Button from "./ui/Button";
+import CartContex from "../store/CartContext";
 import UserProgressContex from "../store/UserProgressContex";
+import { currencyFormatter } from "../utils/formatting";
+import Input from "./ui/Input";
 
 export default function Checkout() {
+  const { items } = useContext(CartContex);
   const { progress, hideCheckout } = useContext(UserProgressContex);
+
+  const totalAmount = items.reduce((total, item) => total + (item.price * item.quantity), 0);
 
   return (
     <Modal open={progress === 'checkout'}>
-      <h2>Checkout</h2>
-      <div className="modal-actions">
-        <Button textOnly onClick={() => hideCheckout()}>Close</Button>
-      </div>
+      <form>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(totalAmount)}</p>
+        <Input id="full-name" label="Full Name" type="text" required />
+        <Input id="email" label="E-mail Address" type="email" required />
+        <Input id="street" label="Street" type="text" required />
+        <div className="control-row">
+          <Input id="postal-code" label="Postal Code" type="text" required />
+          <Input id="city" label="City" type="text" required />
+        </div>
+        <p className="modal-actions">
+          <Button onClick={() => hideCheckout()} type="buton" textOnly>
+            Close
+          </Button>
+          <Button onClick={() => { }}>
+            Submit Order
+          </Button>
+        </p>
+      </form>
     </Modal>
   );
 }
